b9164fdb9319356d370fda31bf718ed1b9e2606b
thejasreerajup <thejasreerajup@gmail.com>
2024-06-22 23:34:53 +0530

    url_categorised

--- turfapp/urls.py
+++ turfapp/urls.py
@@ -2,21 +2,33 @@ from django.urls import path
 from turfapp import views
 
 urlpatterns = [
+
+# Home Pages
 path('',views.index),
 path('gallery/',views.gallery),
 path('event/',views.event),
 path('contact/',views.contact),
 path('about/',views.about),
-path('register/',views.register),
+
+# User Pages
 path('login/',views.login),
+path('turf/',views.turf),
+path('turf_details/',views.turf_details),
+
+# Admin Pages
 path('adminlogin/',views.adminlogin),
 path('addturf/',views.addturf),
 path('userlist/',views.userlist),
 path('adminindex/',views.adminindex),
-path('turf/',views.turf),
-path('turf_details/',views.turf_details),
+path('adminviewbooking/',views.adminviewbooking),
+
+# Backend Actions
+path('register/',views.register),
 path('logout/',views.logout),
 path('booking/',views.booking),
-path('adminviewbooking/',views.adminviewbooking),
-path('approved_booking/',views.approved_booking)
+path('adminlogout/',views.adminlogout),
+path('approved_booking/',views.approved_booking),
+
+# Test Pages
+path('test/',views.test),
 ]
